215d92fdde1f3b3d1bb976a534a7b351bbab4aa0
Nqadhafi <qadhafi817@gmail.com>
2025-02-22 11:26:15 +0700

    add informasi karyawan

--- resources/views/pengambilan/index.blade.php
+++ resources/views/pengambilan/index.blade.php
@@ -12,12 +12,16 @@
 <div class="container mt-4">
     <div class="d-flex justify-content-between align-items-center">
         <h3>Pengambilan Stok</h3>
+
         <div>
             <a href="{{ route('pengambilan.history') }}" class="btn btn-info">Lihat History</a>
             <a href="{{ route('pengambilan.logout') }}" class="btn btn-danger">Logout</a>
         </div>
     </div>
-
+    <div class="mx-auto my-2 alert alert-info">
+        <strong>Nama Karyawan:</strong> {{ $karyawan->nama }} <br>
+        <strong>NIK:</strong> {{ $karyawan->nik }}
+    </div>
     <hr>
 
     <div class="row justify-content-center">
